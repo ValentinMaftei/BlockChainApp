efd787e27f35b167bd0df61899162289b5bb728f
MafteiValentin <maftei_valentin06@yahoo.com>
2024-05-13 01:30:58 +0300

    mESAAAJ

--- contracts/Ticket.sol
+++ contracts/Ticket.sol
@@ -6,6 +6,27 @@ pragma solidity ^0.5.0;
 contract Ticket{
     uint public ticketCount = 0;
 
+    struct TicketStruct{
+        uint id;
+        string name;
+        string description;
+        uint price;
+        address payable owner;
+        bool purchased;
+    }
+
+    mapping(uint => TicketStruct) public tickets;
+
+    constructor() public{
+        createTicket("Coldplay", "Concert Arena Nationala", 10);
+    }
+
+    function createTicket(string memory _name, string memory _description, uint _price) public{
+        ticketCount++;
+        tickets[ticketCount] = TicketStruct(ticketCount, _name, _description, _price, msg.sender, false);
+    }
+
+
 }
 
 
